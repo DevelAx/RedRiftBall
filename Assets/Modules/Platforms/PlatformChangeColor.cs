d5bda184b20f4943406c1dd961a429e6f9767147
Sergey Fisun <develax@gmail.com>
2019-09-13 16:14:01 +0300

    * Floor & platforms colors changed.

--- Assets/Modules/Platforms/PlatformChangeColor.cs
+++ Assets/Modules/Platforms/PlatformChangeColor.cs
@@ -11,7 +11,6 @@ public class PlatformChangeColor : MonoBehaviour
     private void Awake()
     {
         _spriteRenderer = GetComponent<SpriteRenderer>();
-        ChangeColor();
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
